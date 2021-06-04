f184ce868a04531c4f1e92a1f712744b51e22961
psj8252 <psj8252@naver.com>
2021-06-04 17:10:03 +0900

    style: golint 적용

--- urlmap.go
+++ urlmap.go
@@ -4,52 +4,51 @@ import (
 	"sync"
 )
 
-type UrlMap struct {
+// URLMap 은 LongURL와 ShortURL을 맵핑할 수 있는 map입니다.
+type URLMap struct {
 	shortToLong, longToShort map[string]string
-	shortUrlLength           uint
+	shortURLLength           uint
 	sync.Mutex
 }
 
-func (urlmap *UrlMap) getLongUrl(shortUrl string) (string, bool) {
-	longUrl, ok := urlmap.shortToLong[shortUrl]
-	return longUrl, ok
+func (urlmap *URLMap) getLongURL(shortURL string) (string, bool) {
+	longURL, ok := urlmap.shortToLong[shortURL]
+	return longURL, ok
 }
-func (urlmap *UrlMap) getShortUrl(shortUrl string) (string, bool) {
-	shortUrl, ok := urlmap.shortToLong[shortUrl]
-	return shortUrl, ok
+func (urlmap *URLMap) getShortURL(shortURL string) (string, bool) {
+	shortURL, ok := urlmap.shortToLong[shortURL]
+	return shortURL, ok
 }
 
-func (urlmap *UrlMap) PutUrl(longUrl string) (string, bool) {
-	if _, ok := urlmap.longToShort[longUrl]; ok {
+func (urlmap *URLMap) putURL(longURL string) (string, bool) {
+	if _, ok := urlmap.longToShort[longURL]; ok {
 		return "", false
 	}
 
-	shortCand := generateRandomUrl(urlmap.shortUrlLength)
+	shortCand := generateRandomURL(urlmap.shortURLLength)
 	for {
 		_, ok := urlmap.shortToLong[shortCand]
 		if !ok {
 			break
 		}
-		shortCand = generateRandomUrl(urlmap.shortUrlLength)
+		shortCand = generateRandomURL(urlmap.shortURLLength)
 	}
 
 	urlmap.Lock()
-	urlmap.shortToLong[shortCand] = longUrl
-	urlmap.longToShort[longUrl] = shortCand
+	urlmap.shortToLong[shortCand] = longURL
+	urlmap.longToShort[longURL] = shortCand
 	urlmap.Unlock()
 
 	return shortCand, true
 }
 
-func (urlmap *UrlMap) DelUrl(shortUrl string) bool {
-	if longUrl, ok := urlmap.shortToLong[shortUrl]; !ok {
-		return false
-	} else {
+func (urlmap *URLMap) delURL(shortURL string) bool {
+	if longURL, ok := urlmap.shortToLong[shortURL]; ok {
 		urlmap.Lock()
-		delete(urlmap.shortToLong, shortUrl)
-		delete(urlmap.longToShort, longUrl)
+		delete(urlmap.shortToLong, shortURL)
+		delete(urlmap.longToShort, longURL)
 		urlmap.Unlock()
-
 		return true
 	}
+	return false
 }
